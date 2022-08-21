6efe68d62ec658591b530b3c22383a5522779b3c
Renato Mello <renatomdemello@gmail.com>
2022-08-21 20:49:08 -0300

    Project created.

---
+++ src/main/java/com/renatomellotech/userdept/entities/User.java
@@ -0,0 +1,74 @@
+package com.renatomellotech.userdept.entities;
+
+import java.util.Objects;
+
+public class User {
+
+	private Long id;
+	private String name;
+	private String email;
+	
+	private Department department;
+	
+	public User() {		
+	}
+
+	public User(Long id, String name, String email, Department department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.department = department;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(department, email, id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(department, other.department) && Objects.equals(email, other.email)
+				&& Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}	
+	
+}
